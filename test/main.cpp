9bb8e6e24c033dae334db42da7a2395cb907e490
Jonathan Haylett <jonathan@haylett.dev>
2022-04-24 00:11:02 +0100

    Move main.cpp to tests directory

---
+++ test/main.cpp
@@ -0,0 +1,42 @@
+#include "GamecubeConsole.hpp"
+#include "gamecube_definitions.h"
+
+#include "hardware/pio.h"
+#include "pico/stdlib.h"
+
+#include <stdio.h>
+
+void print_bytes(const char *prefix, uint8_t *bytes, uint len);
+
+int main(void) {
+    set_sys_clock_khz(130000, true);
+
+    stdio_init_all();
+
+    uint joybus_pin = 4;
+
+    GamecubeConsole gc = GamecubeConsole(joybus_pin, pio0);
+    gc_report_t gc_report = default_gc_report;
+
+    // Set up LED
+    bool led = true;
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+    while (true) {
+        gc.WaitForPoll();
+        gc.SendReport(&gc_report);
+
+        // Toggle LED
+        led = !led;
+        gpio_put(PICO_DEFAULT_LED_PIN, led);
+    }
+}
+
+void print_bytes(const char *prefix, uint8_t *bytes, uint len) {
+    printf("%s", prefix);
+    for (int i = 0; i < len; i++) {
+        printf("0x%02x ", bytes[i]);
+    }
+    printf("\n");
+}
