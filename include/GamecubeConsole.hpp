4a75083d2ef6a30ba0974b8359a1768b0da80c7d
Jonathan Haylett <jonathan@haylett.dev>
2022-05-28 19:28:20 +0100

    Change delay constants to static constexpr

--- include/GamecubeConsole.hpp
+++ include/GamecubeConsole.hpp
@@ -4,8 +4,8 @@
 #include "gamecube_definitions.h"
 #include "joybus.h"
 
-#include "hardware/pio.h"
-#include "pico/stdlib.h"
+#include <hardware/pio.h>
+#include <pico/stdlib.h>
 
 class GamecubeConsole {
   public:
@@ -24,12 +24,15 @@ class GamecubeConsole {
         POLL = 0x40,
     };
 
-    static const uint incoming_bit_length_us;
-    static const uint max_command_len;
-    joybus_port_t port;
-    uint receive_timeout_us;
-    uint reset_wait_period_us;
-    absolute_time_t last_command_end;
+    static constexpr uint incoming_bit_length_us = 5;
+    static constexpr uint max_command_bytes = 3;
+    static constexpr uint receive_timeout_us = incoming_bit_length_us * 10;
+    static constexpr uint reset_wait_period_us =
+        (incoming_bit_length_us * 8) * (max_command_bytes - 1) + receive_timeout_us;
+    static constexpr uint64_t reply_delay = incoming_bit_length_us - 1;
+
+    joybus_port_t _port;
+    absolute_time_t _receive_end;
 };
 
 #endif
--- src/GamecubeConsole.cpp
+++ src/GamecubeConsole.cpp
@@ -8,19 +8,12 @@
 #include <pico/stdlib.h>
 #include <pico/time.h>
 
-const uint GamecubeConsole::incoming_bit_length_us = 5;
-const uint GamecubeConsole::max_command_len = 3;
-
 GamecubeConsole::GamecubeConsole(uint pin, PIO pio, int sm, int offset) {
-    receive_timeout_us = incoming_bit_length_us * 10;
-    reset_wait_period_us =
-        (incoming_bit_length_us * 8) * (max_command_len - 1) + receive_timeout_us;
-
-    joybus_port_init(&port, pin, pio, sm, offset);
+    joybus_port_init(&_port, pin, pio, sm, offset);
 }
 
 GamecubeConsole::~GamecubeConsole() {
-    joybus_port_terminate(&port);
+    joybus_port_terminate(&_port);
 }
 
 bool __not_in_flash_func(GamecubeConsole::WaitForPoll)() {
@@ -28,33 +21,31 @@ bool __not_in_flash_func(GamecubeConsole::WaitForPoll)() {
     uint8_t received[2];
     uint received_len;
 
-    uint64_t reply_delay = incoming_bit_length_us - 1;
-
     while (true) {
-        joybus_receive_bytes(&port, received, 1, receive_timeout_us, false);
+        joybus_receive_bytes(&_port, received, 1, receive_timeout_us, false);
 
         switch (received[0]) {
             case RESET:
             case PROBE:
                 // Wait for stop bit before responding.
                 sleep_us(reply_delay);
-                joybus_send_bytes(&port, (uint8_t *)&default_gc_status, sizeof(gc_status_t));
+                joybus_send_bytes(&_port, (uint8_t *)&default_gc_status, sizeof(gc_status_t));
                 break;
             case RECALIBRATE:
             case ORIGIN:
                 // Wait for stop bit before responding.
                 sleep_us(reply_delay);
-                joybus_send_bytes(&port, (uint8_t *)&default_gc_origin, sizeof(gc_origin_t));
+                joybus_send_bytes(&_port, (uint8_t *)&default_gc_origin, sizeof(gc_origin_t));
                 break;
             case POLL:
                 // Poll command is 3 bytes total, so read the next 2 bytes now.
-                received_len = joybus_receive_bytes(&port, received, 2, receive_timeout_us, true);
+                received_len = joybus_receive_bytes(&_port, received, 2, receive_timeout_us, true);
                 // Second byte indicates the reading mode.
                 // TODO: Implement other reading modes
                 if (received_len == 2 && received[0] <= 0x07) {
                     // Set timeout for how long to wait until we can send response because we don't
                     // want to reply before the console is done sending.
-                    last_command_end = make_timeout_time_us(reply_delay);
+                    _receive_end = make_timeout_time_us(reply_delay);
 
                     // Return value of rumble bit (least significant bit in last byte).
                     return received[1] & 0x01;
@@ -64,19 +55,19 @@ bool __not_in_flash_func(GamecubeConsole::WaitForPoll)() {
                 // If we received an invalid command, wait long enough for command
                 // to finish, then reset receiving.
                 sleep_us(reset_wait_period_us);
-                joybus_port_reset(&port);
+                joybus_port_reset(&_port);
         }
     }
 }
 
 void __not_in_flash_func(GamecubeConsole::SendReport)(gc_report_t *report) {
     // Wait for receive timeout to end before responding.
-    while (!time_reached(last_command_end)) {
+    while (!time_reached(_receive_end)) {
         tight_loop_contents();
     }
-    joybus_send_bytes(&port, (uint8_t *)report, sizeof(gc_report_t));
+    joybus_send_bytes(&_port, (uint8_t *)report, sizeof(gc_report_t));
 }
 
 int GamecubeConsole::GetOffset() {
-    return port.offset;
+    return _port.offset;
 }
