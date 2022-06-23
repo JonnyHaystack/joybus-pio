0707f9f4ace983e10615e1ec1852d3de56b8bea0
JonnyHaystack <jonathan@haylett.dev>
2022-06-23 18:27:54 +0000

    Deploying to gh-pages from  @ 24e7347350bdca14048264d298016549b95ce6a2 🚀

--- navtreedata.js
+++ navtreedata.js
@@ -20,13 +20,27 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 @licend  The above is the entire license notice
 for the JavaScript code in this file
 */
-var menudata={children:[
-{text:"Main Page",url:"index.html"},
-{text:"Classes",url:"annotated.html",children:[
-{text:"Class List",url:"annotated.html"},
-{text:"Class Index",url:"classes.html"},
-{text:"Class Members",url:"functions.html",children:[
-{text:"All",url:"functions.html"},
-{text:"Functions",url:"functions_func.html"}]}]},
-{text:"Files",url:"files.html",children:[
-{text:"File List",url:"files.html"}]}]}
+var NAVTREE =
+[
+  [ "My Project", "index.html", [
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions", "functions_func.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"GamecubeConsole_8hpp_source.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['detect_15',['Detect',['../classGamecubeConsole.html#a13e892e094b1c7d3c695cdc11ec6cd62',1,'GamecubeConsole']]]
+];
